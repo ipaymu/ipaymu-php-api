2b2f7137134f04d0497caa2bfac8a6cd85d860ff
arakattack <arakattack>
2020-02-28 12:45:27 +0700

    fix path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
  * Copyright (c) 2019.
  */
 spl_autoload_register(function ($class) {
-    $file = __DIR__ . '/../src/' . strtr($class, '\\', '/') . '.php';
+    $file = __DIR__ . '/../iPaymu/' . strtr($class, '\\', '/') . '.php';
     if (file_exists($file)) {
         require $file;
 
